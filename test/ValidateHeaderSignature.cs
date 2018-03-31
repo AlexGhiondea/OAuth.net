8c0c1d3aa1e79dc23e6b9b87d51cd8f63cf7d989
Alex Ghiondea <alex.ghiondea@gmail.com>
2018-03-31 00:03:49 -0700

    Add more tests to validate the signature

--- test/ValidateHeaderSignature.cs
+++ test/ValidateHeaderSignature.cs
@@ -55,5 +55,41 @@ namespace OAuth.Net.Tests
             var result = th.ComputeOAuthHeader(new System.Net.Http.HttpRequestMessage(HttpMethod.Post, "https://www.foo.com/?Zed=one&Alpha&Beta"), "90e2ef68cf1c4949b7eb10eccbf28ded", "1522478309");
             Assert.Equal("oauth_consumer_key=1234,oauth_nonce=90e2ef68cf1c4949b7eb10eccbf28ded,oauth_timestamp=1522478309,oauth_signature_method=HMAC-SHA1,oauth_version=1.0a,oauth_token=234,oauth_signature=ixPe0T85xtBNamcPP4cvATsgVDE%3D", result);
         }
+        [Fact]
+        public void ValidateSignature8()
+        {
+            TestHelper th = new TestHelper("1234", "4567", "234", "23424243243");
+            var result = th.ComputeOAuthHeader(new System.Net.Http.HttpRequestMessage(HttpMethod.Post, "https://www.foo.com/?Zed=one&Alpha-Is-Allowed&Beta"), "073e9812f10d4356bf0db6049ffc7673", "1522479777");
+            Assert.Equal("oauth_consumer_key=1234,oauth_nonce=073e9812f10d4356bf0db6049ffc7673,oauth_timestamp=1522479777,oauth_signature_method=HMAC-SHA1,oauth_version=1.0a,oauth_token=234,oauth_signature=66iCpYzWSy6kb88J22ZBTjTSvks%3D", result);
+        }
+        [Fact]
+        public void ValidateSignature9()
+        {
+            TestHelper th = new TestHelper("1234", "4567", "234", "23424243243");
+            var result = th.ComputeOAuthHeader(new System.Net.Http.HttpRequestMessage(HttpMethod.Post, "https://www.foo.com/?These_Chars-are.Allowed~here"), "3ae4b47630c94faab2e51e3eb9220d56", "1522479777");
+            Assert.Equal("oauth_consumer_key=1234,oauth_nonce=3ae4b47630c94faab2e51e3eb9220d56,oauth_timestamp=1522479777,oauth_signature_method=HMAC-SHA1,oauth_version=1.0a,oauth_token=234,oauth_signature=B3Rt%2Fz1ssytWlEB5WRVV%2F9nUoa0%3D", result);
+        }
+        [Fact]
+        public void ValidateSignature10()
+        {
+            TestHelper th = new TestHelper("1234", "4567", "234", "23424243243");
+            var result = th.ComputeOAuthHeader(new System.Net.Http.HttpRequestMessage(HttpMethod.Post, "https://www.foo.com/?Escape[these]parameters"), "b8b9b2080c0b4417a4309ba22df51a40", "1522479777");
+            Assert.Equal("oauth_consumer_key=1234,oauth_nonce=b8b9b2080c0b4417a4309ba22df51a40,oauth_timestamp=1522479777,oauth_signature_method=HMAC-SHA1,oauth_version=1.0a,oauth_token=234,oauth_signature=iVmkZgBGLZDU99Sc1MezFnVP5FY%3D", result);
+        }
+        [Fact]
+        public void ValidateSignature11()
+        {
+            TestHelper th = new TestHelper("1234", "4567", "234", "23424243243");
+            var result = th.ComputeOAuthHeader(new System.Net.Http.HttpRequestMessage(HttpMethod.Post, "https://www.foo.com/?Escapeparameters{}"), "b3dffbfed3bf4777a27bb631c9910ae2", "1522479777");
+            Assert.Equal("oauth_consumer_key=1234,oauth_nonce=b3dffbfed3bf4777a27bb631c9910ae2,oauth_timestamp=1522479777,oauth_signature_method=HMAC-SHA1,oauth_version=1.0a,oauth_token=234,oauth_signature=EeNDJAVIIc1BzviHYP%2B7jAl6BDA%3D", result);
+        }
+        [Fact]
+        public void ValidateSignature12()
+        {
+            TestHelper th = new TestHelper("1234", "4567", "234", "23424243243");
+            var result = th.ComputeOAuthHeader(new System.Net.Http.HttpRequestMessage(HttpMethod.Post, "https://www.foo.com/?Escape[these]parameters{}"), "0f6093ebbee04aa4a9cf6c7e3a095702", "1522479777");
+            Assert.Equal("oauth_consumer_key=1234,oauth_nonce=0f6093ebbee04aa4a9cf6c7e3a095702,oauth_timestamp=1522479777,oauth_signature_method=HMAC-SHA1,oauth_version=1.0a,oauth_token=234,oauth_signature=0eQCtR89OhlUPIBqku8PobHIFQA%3D", result);
+        }
+
     }
 }
