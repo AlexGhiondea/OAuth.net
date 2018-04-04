fc3305d7bfa28b15a19957a7ba6ca075ce6c1407
Alex Ghiondea <alex.ghiondea@gmail.com>
2018-04-04 10:28:04 -0700

    Change the tests to not use reflection anymore but instead use the TestHttpMessageHandler

--- test/TestHelper.cs
+++ test/TestHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -8,14 +9,14 @@ namespace OAuth.Net.Tests
     public class TestHelper
     {
         private readonly string _apiKey;
-        private readonly string _secret;
+        private readonly string _clientSecret;
         private readonly string _authToken;
         private readonly string _authTokenSecret;
 
         public TestHelper(string apiKey, string secret, string authToken, string authTokenSecret)
         {
             _apiKey = apiKey;
-            _secret = secret;
+            _clientSecret = secret;
             _authToken = authToken;
             _authTokenSecret = authTokenSecret;
         }
@@ -27,29 +28,35 @@ namespace OAuth.Net.Tests
         /// <returns></returns>
         public string ComputeOAuthSignature(HttpRequestMessage request, string nonce, string timestamp, OAuthVersion version)
         {
-            OAuth.OAuthMessageHandler msgHandler = new OAuthMessageHandler(_apiKey, _secret, _authToken, _authTokenSecret, new TestOAuthProvider(nonce, timestamp, version));
+            OAuthMessageHandler msgHandler = new OAuthMessageHandler(
+                _apiKey,
+                _clientSecret,
+                _authToken,
+                _authTokenSecret,
+                new TestOAuthProvider(
+                    nonce,
+                    timestamp,
+                    version));
 
-            // get access to the method.
-            MethodInfo getAuthHeaderMethod = msgHandler.GetType().GetMethod("GetAuthenticationHeaderForRequest",
-                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance,
-                null,
-                new Type[] { typeof(HttpRequestMessage) },
-                null);
+            return GetOAuthParameterFromHandlerAsync(msgHandler, request,"oauth_signature").Result;
+        }
 
-            Task<string> taskResult = getAuthHeaderMethod.Invoke(msgHandler, new object[] { request }) as Task<string>;
-            string headerString = taskResult.Result;
+        private async Task<string> GetOAuthParameterFromHandlerAsync(OAuthMessageHandler handler, HttpRequestMessage request, string requestedParameter)
+        {
+            var testHandler = new TestHttpMessageHandler();
+            handler.InnerHandler = testHandler;
 
-            // extract the oauth signature
-            string[] elems = headerString.Split(',');
-            foreach (var item in elems)
+            using (var httpClient = new HttpClient(handler, disposeHandler: false))
             {
-                if (item.StartsWith("oauth_signature"))
-                {
-                    return item.Split('=')[1];
-                }
+                await httpClient.SendAsync(request);
             }
 
-            return null;
+            var request2 = testHandler.SentMessages.Single();
+            var authHeaderParameter = request.Headers.Authorization.Parameter;
+
+            // extract the parameter
+            string[] elems = authHeaderParameter.Split(',');
+            return elems.SingleOrDefault(x => x.StartsWith($"{requestedParameter}="))?.Split('=')[1];
         }
     }
 }
