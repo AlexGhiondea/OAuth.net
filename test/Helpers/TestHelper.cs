3e3bec4aacde6a85b53a5a7ff3ff51d07a7691d2
Alex Ghiondea <alex.ghiondea@gmail.com>
2018-06-04 12:57:23 -0700

    Update the tests to account for the fact that oauth headers are between double quotes
    The code that extracts the value of the oauth parameter is updated to validate the presence of the double quotes and strip them before returing the value of the header.
    With this change, tests don't have to validate the parameter is between double quotes and instead can validate the actual value directly.

--- test/Helpers/TestHelper.cs
+++ test/Helpers/TestHelper.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Reflection;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace OAuth.Net.Tests
 {
@@ -92,7 +93,22 @@ namespace OAuth.Net.Tests
 
             // extract the parameter
             string[] elems = authHeaderParameter.Split(',');
-            return elems.SingleOrDefault(x => x.StartsWith($"{requestedParameter}="))?.Split('=')[1];
+            string parameter = elems.SingleOrDefault(x => x.StartsWith($"{requestedParameter}="))?.Split('=')[1];
+
+            // we might not have the parameter here.
+            if (parameter == null)
+                return null;
+
+            // we are now ensuring that the parameters are contained within double quotes.
+            Assert.Equal('"', parameter[0]);
+            Assert.Equal('"', parameter[parameter.Length - 1]);
+
+            // if we only have 2 characters, they are both double quotes, so return empty string
+            if (parameter.Length == 2)
+                return string.Empty;
+
+            // for the purposes of the test, extract the double quotes
+            return parameter.Substring(1, parameter.Length - 2);
         }
     }
 }
